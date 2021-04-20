69edb4a06fafa5b11606673b8118d0e0eee50bfe
rudenko_diana <dianarudenko@yandex.ru>
2021-04-20 15:49:07 +0300

    check point

---
+++ src/main/controller/MainController.java
@@ -0,0 +1,15 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.ui.ModelMap;
+
+@Controller
+public class MainController {
+   @RequestMapping(value="/", method = RequestMethod.GET)
+   public String printHello(ModelMap model) {
+      model.addAttribute("message", "Hello Spring MVC Framework!");
+      return "index";
+   }
+}
---
+++ src/main/web/WEB-INF/pages/index.jsp
@@ -0,0 +1,12 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<!-- <%@ include file="/WEB-INF/view/includes.jsp" %> -->
+
+<!DOCTYPE html>
+<html>
+    <title>
+        My super project!
+    </title>
+    <body>
+        It's alive!
+    </body>
+</html>
